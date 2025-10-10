d8a729b4cdb0b8f5434c27cee030a108ddd87edd
Olegg-22 <o.zaichikov@g.nsu.ru>
2025-10-11 06:19:06 +0700

    исправил замечания

--- lab1/main.go
+++ lab1/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"flag"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 )
 
 const (
 	countByteId           = 4
-	localhost             = "127.0.0.1"
+	localhostIPv4         = "127.0.0.1"
+	localhostIPv6         = "::1"
 	sizeBuffer            = 128
 	countReceivedArgument = 2
 )
@@ -32,24 +38,51 @@ type InfoStruct struct {
 	selfAddr    string
 	connections map[string]Connection
 	mu          sync.Mutex
+	printCh     chan struct{}
+	ctx         context.Context
+	cancel      context.CancelFunc
+	wg          sync.WaitGroup
 }
 
-func newInfoStruct(group string) (*InfoStruct, error) {
+func newInfoStruct(group string, preferredIface string) (*InfoStruct, error) {
 	gaddr, err := net.ResolveUDPAddr("udp", group)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("ResolveUDPAddr error: %w", err)
 	}
 
+	useIPv6 := gaddr.IP.To4() == nil
 	network := "udp4"
-	if gaddr.IP.To4() == nil {
+	if useIPv6 {
 		network = "udp6"
 	}
+
+	hasAddrFor := func(i *net.Interface) bool {
+		return len(ifaceValidIPs(i, useIPv6)) > 0
+	}
+
 	var iface *net.Interface
-	ifaces, _ := net.Interfaces()
-	for _, i := range ifaces {
-		if (i.Flags&net.FlagUp) != 0 && (i.Flags&net.FlagMulticast) != 0 {
-			iface = &i
-			break
+	if preferredIface != "" {
+		i, err := net.InterfaceByName(preferredIface)
+		if err != nil {
+			return nil, fmt.Errorf("interface %s not found: %w", preferredIface, err)
+		}
+		if (i.Flags&net.FlagUp) == 0 || (i.Flags&net.FlagMulticast) == 0 {
+			return nil, fmt.Errorf("interface %s is not up or doesn't support multicast", preferredIface)
+		}
+		if !hasAddrFor(i) {
+			return nil, fmt.Errorf("interface %s does not have an address for the required IPs", preferredIface)
+		}
+		iface = i
+	} else {
+		ifaces, _ := net.Interfaces()
+		for _, i := range ifaces {
+			if len(ifaceValidIPs(&i, useIPv6)) > 0 {
+				iface = &i
+				if useIPv6 {
+					gaddr.Zone = i.Name
+				}
+				break
+			}
 		}
 	}
 	if iface == nil {
@@ -61,14 +94,25 @@ func newInfoStruct(group string) (*InfoStruct, error) {
 		return nil, fmt.Errorf("listen multicast: %w", err)
 	}
 
-	localAddr := &net.UDPAddr{IP: ifaceLocalIP(iface), Port: 0}
+	localIP := ifaceLocalIP(iface, useIPv6)
+	localAddr := &net.UDPAddr{IP: localIP, Port: 0}
+	if useIPv6 && localIP.IsLinkLocalUnicast() {
+		localAddr.Zone = iface.Name
+	}
 	send, err := net.DialUDP(network, localAddr, gaddr)
 	if err != nil {
 		return nil, fmt.Errorf("dial udp: %w", err)
 	}
 
-	id := randomID()
-	selfIP := ifaceLocalIP(iface).String()
+	id, err := randomID()
+	if err != nil {
+		return nil, fmt.Errorf("generate id: %w", err)
+	}
+	selfIP := localIP.String()
+	if useIPv6 && localIP.IsLinkLocalUnicast() {
+		selfIP = localIP.String() + "%" + iface.Name
+	}
+	ctx, cancel := context.WithCancel(context.Background())
 
 	return &InfoStruct{
 		group:       gaddr,
@@ -77,45 +121,122 @@ func newInfoStruct(group string) (*InfoStruct, error) {
 		id:          id,
 		selfAddr:    selfIP,
 		connections: make(map[string]Connection),
+		printCh:     make(chan struct{}, 1),
+		ctx:         ctx,
+		cancel:      cancel,
 	}, nil
 }
 
-func randomID() string {
+func randomID() (string, error) {
 	b := make([]byte, countByteId)
-	rand.Read(b)
-	return hex.EncodeToString(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), err
 }
 
-func ifaceLocalIP(iface *net.Interface) net.IP {
+func ifaceValidIPs(iface *net.Interface, useIPv6 bool) []net.IP {
 	addrs, _ := iface.Addrs()
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			return ipnet.IP
+	var result []net.IP
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok {
+			if useIPv6 {
+				if ipnet.IP.To4() == nil && !ipnet.IP.IsLoopback() {
+					result = append(result, ipnet.IP)
+				}
+			} else {
+				if ip := ipnet.IP.To4(); ip != nil && !ip.IsLoopback() {
+					result = append(result, ip)
+				}
+			}
 		}
 	}
-	return net.ParseIP(localhost)
+	return result
+}
+
+func ifaceLocalIP(iface *net.Interface, useIPv6 bool) net.IP {
+	ips := ifaceValidIPs(iface, useIPv6)
+	if len(ips) > 0 {
+		return ips[0]
+	}
+	if useIPv6 {
+		return net.ParseIP(localhostIPv6)
+	}
+	return net.ParseIP(localhostIPv4)
 }
 
 func (lib *InfoStruct) run() {
-	go lib.sendConnect()
-	go lib.receive()
-	go lib.cleanup()
+	lib.wg.Add(1)
+	go func() {
+		defer lib.wg.Done()
+		lib.sendConnect(lib.ctx)
+	}()
+
+	lib.wg.Add(1)
+	go func() {
+		defer lib.wg.Done()
+		lib.receive(lib.ctx)
+	}()
+
+	lib.wg.Add(1)
+	go func() {
+		defer lib.wg.Done()
+		lib.cleanup(lib.ctx)
+	}()
+
+	lib.wg.Add(1)
+	go func() {
+		defer lib.wg.Done()
+		lib.printConnections(lib.ctx)
+	}()
 }
 
-func (lib *InfoStruct) sendConnect() {
+func (lib *InfoStruct) sendConnect(ctx context.Context) {
 	t := time.NewTicker(2 * time.Second)
 	defer t.Stop()
-	for range t.C {
-		msg := []byte("CONNECT:" + lib.id)
-		lib.sendConn.Write(msg)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			msg := []byte("CONNECT:" + lib.id)
+			if lib.sendConn == nil {
+				continue
+			}
+			if _, err := lib.sendConn.Write(msg); err != nil {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				fmt.Println("send error:", err)
+			}
+		}
 	}
 }
 
-func (lib *InfoStruct) receive() {
+func (lib *InfoStruct) receive(ctx context.Context) {
 	buf := make([]byte, sizeBuffer)
 	for {
+		_ = lib.recvConn.SetReadDeadline(time.Now().Add(1 * time.Second))
 		n, src, err := lib.recvConn.ReadFromUDP(buf)
 		if err != nil {
+			var ne net.Error
+			if errors.As(err, &ne) && ne.Timeout() {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					continue
+				}
+			}
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			fmt.Println("read error:", err)
 			continue
 		}
 		msg := string(buf[:n])
@@ -129,57 +250,108 @@ func (lib *InfoStruct) receive() {
 		}
 		lib.mu.Lock()
 		lib.connections[connectionID] = Connection{ID: connectionID, Addr: src.IP.String(), LastSeen: time.Now()}
-		lib.printConnections()
 		lib.mu.Unlock()
+
+		select {
+		case lib.printCh <- struct{}{}:
+		default:
+		}
 	}
 }
 
-func (lib *InfoStruct) cleanup() {
+func (lib *InfoStruct) cleanup(ctx context.Context) {
 	t := time.NewTicker(3 * time.Second)
 	defer t.Stop()
-	for range t.C {
-		now := time.Now()
-		lib.mu.Lock()
-		changed := false
-		for id, p := range lib.connections {
-			if now.Sub(p.LastSeen) > 6*time.Second {
-				delete(lib.connections, id)
-				changed = true
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			now := time.Now()
+			changed := false
+			lib.mu.Lock()
+			for id, p := range lib.connections {
+				if now.Sub(p.LastSeen) > 6*time.Second {
+					delete(lib.connections, id)
+					changed = true
+				}
+			}
+			lib.mu.Unlock()
+
+			if changed {
+				select {
+				case lib.printCh <- struct{}{}:
+				default:
+				}
 			}
 		}
-		if changed {
-			lib.printConnections()
-		}
-		lib.mu.Unlock()
 	}
 }
 
-func (lib *InfoStruct) printConnections() {
-	fmt.Println("===================================")
-	fmt.Printf("🟢 Myself: %s (%s)\n", lib.id, lib.selfAddr)
-	fmt.Println("Connections:")
-	if len(lib.connections) == 0 {
-		fmt.Println("  (none)")
-	} else {
-		for _, p := range lib.connections {
-			fmt.Printf("  🔸 %s (%s)\n", p.ID, p.Addr)
+func (lib *InfoStruct) printConnections(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-lib.printCh:
+			lib.mu.Lock()
+			id := lib.id
+			selfAddr := lib.selfAddr
+			conns := make([]Connection, 0, len(lib.connections))
+			for _, c := range lib.connections {
+				conns = append(conns, c)
+			}
+			lib.mu.Unlock()
+
+			fmt.Println("===================================")
+			fmt.Printf("🟢 Myself: %s (%s)\n", id, selfAddr)
+			fmt.Println("Connections:")
+			if len(conns) == 0 {
+				fmt.Println("  (none)")
+			} else {
+				for _, p := range conns {
+					fmt.Printf("  🔸 %s (%s)\n", p.ID, p.Addr)
+				}
+			}
+			fmt.Println("===================================")
 		}
 	}
-	fmt.Println("===================================")
 }
+
+func (lib *InfoStruct) Shutdown() {
+	lib.cancel()
+
+	if lib.recvConn != nil {
+		_ = lib.recvConn.Close()
+	}
+	if lib.sendConn != nil {
+		_ = lib.sendConn.Close()
+	}
+
+	lib.wg.Wait()
+}
+
+var ifaceName = flag.String("iface", "", "network interface to use (optional)")
 
 func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
-		fmt.Println("Usage: go run main.go <multicastAddr:port>")
+		fmt.Println("Usage: go run main.go [-iface en0] <multicastAddr:port>")
 		return
 	}
 	group := flag.Arg(0)
-	lib, err := newInfoStruct(group)
+	lib, err := newInfoStruct(group, *ifaceName)
 	if err != nil {
 		fmt.Println("error:", err)
 		return
 	}
 	lib.run()
-	select {}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigCh
+	fmt.Println("\n===SHUTDOWN...===")
+	lib.Shutdown()
+	fmt.Println("\n===SHUTDOWN END===")
 }
